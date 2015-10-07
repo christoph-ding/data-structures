70b68b8796ab9563e6fb8aab9464fde856e34a46
ironpup <christoph.ding@gmail.com>
2015-10-07 14:35:34 -0700

    "Completed Function Pattern for Stack"

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var length = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    length++;
+    storage[length] = value;
   };
 
   someInstance.pop = function() {
+    var result;
+    if (length > 0) {
+      result = storage[length];
+      delete storage[length];
+      length--;
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
