8b6647e17c7b9acd8df04d5df33522e5dfdf952b
ironpup <christoph.ding@gmail.com>
2015-10-07 15:47:36 -0700

    completed functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,35 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var length = 0;
+  var tail = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    length++;
+    tail++;
+    storage[tail] = value;
   };
 
   someInstance.dequeue = function() {
+    var result;
+    if (length > 0) {
+      var head = _.reduce(storage, function(smallestSoFar, value, key) {
+        if (key < smallestSoFar) {
+          smallestSoFar = key;
+        }
+        return smallestSoFar;
+      }, tail);
+      // sort storage by index key, head is smallest index key
+      result = storage[head];
+      delete storage[head];
+      length--;
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
