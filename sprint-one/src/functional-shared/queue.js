c3daa093475c01a32f9e75c6f91dbd26fab52bc8
ironpup <christoph.ding@gmail.com>
2015-10-07 20:44:51 -0700

    Implemented functional-shared Stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,20 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.length = 0;
+
+
+
+
+
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function() {
+  
+}
 
 
