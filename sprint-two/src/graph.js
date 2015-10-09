5fc83e7869f5fdd0e7ab312cc804639aedb2b72d
ironpup <christoph.ding@gmail.com>
2015-10-09 11:24:08 -0700

    Implemented Set

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -9,18 +9,21 @@ var Graph = function() {
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
+  // O(1)
 Graph.prototype.addNode = function(node) {
   this.nodes.push(node);
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
+  // O(n)
 Graph.prototype.contains = function(node) {
   return _.contains(this.nodes, node);
 };
 
 // ------------------------
 // Removes a node from the graph.
+  // O(n)
 Graph.prototype.removeNode = function(node) {
   _.each(this.nodes, function(currentNode, index, nodes) {
     if (currentNode === node) {
@@ -31,6 +34,7 @@ Graph.prototype.removeNode = function(node) {
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+  // O(n)
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   var result = false;
   _.each(this.edges, function(edge) {
@@ -43,6 +47,7 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
+  // O(1)
 Graph.prototype.addEdge = function(fromNode, toNode) {
   if(this.contains(fromNode) && this.contains(toNode)) {
     var edgeOne = {};
@@ -55,7 +60,8 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 };
 
 // ------------------------
-// Remove an edge between any two specified (by value) nodes.
+// Remove an edge between any two specified (by value) nodes. 
+  // O(n)
 Graph.prototype.removeEdge = function(fromNode, toNode) {
   var thingsToSplice;
   _.each(this.edges, function(edge, index, list) {
@@ -79,6 +85,7 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
+  // O(n)
 Graph.prototype.forEachNode = function(cb) {
   _.each(this.nodes, cb);
 };
